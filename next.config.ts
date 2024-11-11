a6f5107d1dcdcff94714c8eae9e0f77906f1d16e
Alex Brizhevatiy <alverill@gmail.com>
2024-11-11 20:10:24 +0200

    qwe

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["v5.airtableusercontent.com"],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,  // якщо також хочете ігнорувати помилки ESLint
+  },
 };
 
 export default nextConfig;
